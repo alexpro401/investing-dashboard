8b048d6aa23fb5d8265fc40c9085751bd7bc2c0b
Saymon <artem.simonenko@dexe.network>
2022-08-13 11:57:21 +0300

    Fund fee page

--- src/pages/FundDetailsFee/index.tsx
+++ src/pages/FundDetailsFee/index.tsx
@@ -4,40 +4,41 @@ import { createClient, Provider as GraphProvider } from "urql"
 import { useWeb3React } from "@web3-react/core"
 import { BigNumber, FixedNumber } from "@ethersproject/bignumber"
 import { useSelector } from "react-redux"
+import { format } from "date-fns"
 
-import { formatBigNumber } from "utils"
-import { usePoolContract, usePoolQuery } from "hooks/usePool"
+import { PriceFeed } from "abi"
+import useCoreProperties from "hooks/useCoreProperties"
+import { expandTimestamp, formatBigNumber } from "utils"
 import useContract, { useERC20 } from "hooks/useContract"
-import { TraderPool, PriceFeed } from "abi"
 import { usePoolMetadata } from "state/ipfsMetadata/hooks"
-import { selectPriceFeedAddress } from "state/contracts/selectors"
 import useWithdrawalHistory from "hooks/useWithdrawalHistory"
+import { selectPriceFeedAddress } from "state/contracts/selectors"
+import { usePoolContract, usePoolQuery, useTraderPool } from "hooks/usePool"
 
 import { Flex } from "theme"
-
 import Icon from "components/Icon"
+import Amount from "components/Amount"
 import Button from "components/Button"
 import FeeChart from "components/FeeChart"
 import Accordion from "components/Accordion"
-import Amount from "components/Amount"
 import AmountRow from "components/Amount/Row"
-
 import WithdrawalHistory from "./WithdrawalHistory"
 
-import {
-  PageLoading,
-  Container,
-  FeeDateCard,
-  FeeDateText,
-  MainCard,
-  MainCardTitle,
-  MainCardDescription,
-  MainCardHeaderRight,
-} from "./styled"
+import S, { PageLoading } from "./styled"
 
 const poolsClient = createClient({
   url: process.env.REACT_APP_ALL_POOLS_API_URL || "",
 })
+
+interface IAmount {
+  big: BigNumber
+  format: string
+}
+
+const defaultAmountState: IAmount = {
+  big: BigNumber.from("0"),
+  format: formatBigNumber(BigNumber.from("0"), 18, 6),
+}
 
 const FundDetailsFee: FC = () => {
   const { poolAddress } = useParams()
@@ -46,58 +47,188 @@ const FundDetailsFee: FC = () => {
   const [poolData] = usePoolQuery(poolAddress)
   const [, poolInfoData] = usePoolContract(poolAddress)
   const [, baseToken] = useERC20(poolData?.baseToken)
-
-  const traderPool = useContract(poolData?.id, TraderPool)
+  const traderPool = useTraderPool(poolData?.id)
 
   const priceFeedAddress = useSelector(selectPriceFeedAddress)
   const priceFeed = useContract(priceFeedAddress, PriceFeed)
+  const coreProperties = useCoreProperties()
 
   const [{ poolMetadata }] = usePoolMetadata(
     poolAddress,
     poolInfoData?.parameters.descriptionURL
   )
 
-  const [platformCommissionBase, setPlatformCommissionBase] = useState("0")
-  const [traderCommissionBase, setTraderCommissionBase] = useState("0")
-  const [fundsUnderManagementDexe, setFundsUnderManagementDexe] = useState("0")
+  const [traderDexeManagementAmount, setTraderDexeManagementAmount] =
+    useState<IAmount>(defaultAmountState)
+  const [traderUSDCommission, setTraderUSDCommission] =
+    useState<IAmount>(defaultAmountState)
+  const [traderBaseCommission, setTraderBaseCommission] =
+    useState<IAmount>(defaultAmountState)
+
+  const [platformLPCommission, setPlatformLPCommission] =
+    useState<IAmount>(defaultAmountState)
+  const [platformUSDCommission, setPlatformUSDCommission] =
+    useState<IAmount>(defaultAmountState)
+  const [platformPercentageCommission, setPlatformPercentageCommission] =
+    useState<IAmount>(defaultAmountState)
+
+  const [netInvestorProfitDEXE, setNetInvestorProfitDEXE] =
+    useState<IAmount>(defaultAmountState)
+
+  const [unlockTimestamp, setUnlockTimestamp] = useState<BigNumber>(
+    BigNumber.from("0")
+  )
 
   const withdrawalHistory = useWithdrawalHistory()
 
-  console.groupCollapsed("poolData")
-  console.log("Subgraph poolData", poolData)
-  console.log("Contranct getPoolInfo", poolInfoData)
-  console.groupEnd()
+  /**
+   * Next commission epoch start date
+   */
+  const unlockDate = useMemo<string>(() => {
+    if (!unlockTimestamp || unlockTimestamp.isZero()) return ""
 
-  const fundsUnderManagementBase = useMemo(() => {
-    if (!poolInfoData) return "0"
+    const expanded = expandTimestamp(Number(unlockTimestamp.toString()))
+    return format(expanded, "MMM dd, y")
+  }, [unlockTimestamp])
 
-    const { totalPoolBase, traderBase } = poolInfoData
+  /**
+   * Total funds under pool management in USD
+   */
+  const totalPoolUSD = useMemo<IAmount>(() => {
+    if (!poolInfoData) return defaultAmountState
 
-    const _totalPoolBase = FixedNumber.fromValue(totalPoolBase, 18)
-    const _traderBase = FixedNumber.fromValue(traderBase, 18)
-
-    const result = _totalPoolBase.subUnsafe(_traderBase)
-
-    return BigNumber.from(result)
+    return {
+      big: poolInfoData.totalPoolUSD,
+      format: formatBigNumber(poolInfoData.totalPoolUSD, 18, 2),
+    }
   }, [poolInfoData])
 
-  const commissionPercentage = useMemo(() => {
-    if (!poolInfoData) return "0"
-
-    return formatBigNumber(poolInfoData?.parameters.commissionPercentage, 25, 0)
-  }, [poolInfoData])
-
-  const totalPoolUSD = useMemo(() => {
-    if (!poolInfoData) return "0"
-
-    return formatBigNumber(poolInfoData.totalPoolUSD, 18, 2)
-  }, [poolInfoData])
-
-  const totalPoolBase = useMemo(() => {
+  /**
+   * Total funds under pool management in baseToken
+   */
+  const totalPoolBase = useMemo<string>(() => {
     if (!poolInfoData) return "0"
 
     return formatBigNumber(poolInfoData.totalPoolBase)
   }, [poolInfoData])
+
+  /**
+   * Funds under pool management in baseToken (without trader funds)
+   */
+  const fundsUnderManagementBase = useMemo<BigNumber>(() => {
+    if (!poolInfoData) return BigNumber.from("0")
+
+    const { totalPoolBase, traderBase } = poolInfoData
+
+    const result = FixedNumber.fromValue(totalPoolBase, 18).subUnsafe(
+      FixedNumber.fromValue(traderBase, 18)
+    )
+
+    return BigNumber.from(result)
+  }, [poolInfoData])
+
+  /**
+   * Pool commission percentage
+   */
+  const poolCommissionPercentage = useMemo<IAmount>(() => {
+    if (!poolInfoData) return defaultAmountState
+    const { commissionPercentage } = poolInfoData?.parameters
+    return {
+      big: commissionPercentage,
+      format: formatBigNumber(commissionPercentage, 25, 2),
+    }
+  }, [poolInfoData])
+
+  /**
+   * Pool profit (without trader funds)
+   * (platformUSDCommission + traderUSDCommission) / (100% - poolCommissionPercentage)
+   */
+  const poolProfitWithoutTraderUSD = useMemo<IAmount>(() => {
+    if (
+      !platformUSDCommission ||
+      !traderUSDCommission ||
+      !poolCommissionPercentage
+    ) {
+      return defaultAmountState
+    }
+
+    const commissionsSum = FixedNumber.fromValue(
+      platformUSDCommission.big,
+      18
+    ).addUnsafe(FixedNumber.fromValue(traderUSDCommission.big, 18))
+
+    const percent = FixedNumber.from("100").subUnsafe(
+      FixedNumber.fromValue(poolCommissionPercentage.big, 18)
+    )
+
+    const big = BigNumber.from(commissionsSum.divUnsafe(percent))
+
+    return { big, format: formatBigNumber(big, 18, 6) }
+  }, [platformUSDCommission, traderUSDCommission, poolCommissionPercentage])
+
+  /**
+   * Investors profit in USD
+   * fundsUnderManagementBase - (platformUSDCommission + traderUSDCommission)
+   */
+  const netInvestorProfitUSD = useMemo<IAmount>(() => {
+    if (
+      !fundsUnderManagementBase ||
+      !platformUSDCommission.big ||
+      !traderUSDCommission.big
+    ) {
+      return defaultAmountState
+    }
+
+    const platformAndTraderAmount = FixedNumber.fromValue(
+      platformUSDCommission.big,
+      18
+    ).addUnsafe(FixedNumber.fromValue(traderUSDCommission.big, 18))
+
+    const big = BigNumber.from(
+      FixedNumber.fromValue(fundsUnderManagementBase, 18).subUnsafe(
+        platformAndTraderAmount
+      )
+    )
+
+    return { big, format: formatBigNumber(big, 18, 2) }
+  }, [
+    fundsUnderManagementBase,
+    platformUSDCommission.big,
+    traderUSDCommission.big,
+  ])
+
+  /**
+   * Investors profit percentage
+   * (netInvestorProfitUSD / (totalPoolUSD - traderUSDCommission) ) / 100
+   */
+  const netInvestorProfitPercentage = useMemo<IAmount>(() => {
+    if (
+      !netInvestorProfitUSD.big ||
+      !totalPoolUSD.big ||
+      !traderUSDCommission.big ||
+      netInvestorProfitUSD.big.isZero() ||
+      totalPoolUSD.big.isZero() ||
+      traderUSDCommission.big.isZero()
+    ) {
+      return defaultAmountState
+    }
+
+    const investorsUSDAmount = FixedNumber.fromValue(
+      totalPoolUSD.big,
+      18
+    ).subUnsafe(FixedNumber.fromValue(traderUSDCommission.big, 18))
+
+    const investorsUSDSome = FixedNumber.fromValue(
+      netInvestorProfitUSD.big,
+      18
+    ).divUnsafe(investorsUSDAmount)
+
+    const big = BigNumber.from(
+      investorsUSDSome.divUnsafe(FixedNumber.from(100))
+    )
+
+    return { big, format: formatBigNumber(big, 18, 2) }
+  }, [netInvestorProfitUSD, totalPoolUSD, traderUSDCommission])
 
   const baseTokenSymbol = useMemo(() => {
     if (!baseToken) return ""
@@ -112,29 +243,55 @@ const FundDetailsFee: FC = () => {
     console.log("Request Performance Fee")
   }
 
+  // Fetch platform and trader commissions
   useEffect(() => {
     if (!traderPool) return
     ;(async () => {
       try {
-        const commissions = await traderPool.getReinvestCommissions([0, 1])
+        const commissions = await traderPool.getReinvestCommissions([0, 1000])
 
-        if (commissions.dexeBaseCommission) {
-          const platformCommission = formatBigNumber(
-            commissions.dexeBaseCommission,
-            18,
-            6
-          )
-          setPlatformCommissionBase(platformCommission)
+        if (commissions.dexeLPCommission) {
+          const { dexeLPCommission } = commissions
+
+          const prepared: IAmount = {
+            big: dexeLPCommission,
+            format: formatBigNumber(dexeLPCommission, 18, 6),
+          }
+
+          setPlatformLPCommission(prepared)
+        }
+
+        if (commissions.dexeUSDCommission) {
+          const { dexeUSDCommission } = commissions
+
+          const prepared: IAmount = {
+            big: dexeUSDCommission,
+            format: formatBigNumber(dexeUSDCommission, 18, 2),
+          }
+
+          setPlatformUSDCommission(prepared)
         }
 
         if (commissions.traderBaseCommission) {
-          const traderCommission = formatBigNumber(
-            commissions.traderBaseCommission,
-            18,
-            6
-          )
+          const { traderBaseCommission } = commissions
 
-          setTraderCommissionBase(traderCommission)
+          const prepared: IAmount = {
+            big: traderBaseCommission,
+            format: formatBigNumber(traderBaseCommission, 18, 2),
+          }
+
+          setTraderBaseCommission(prepared)
+        }
+
+        if (commissions.traderUSDCommission) {
+          const { traderUSDCommission } = commissions
+
+          const prepared: IAmount = {
+            big: traderUSDCommission,
+            format: formatBigNumber(traderUSDCommission, 18, 2),
+          }
+
+          setTraderUSDCommission(prepared)
         }
       } catch (error) {
         console.error(error)
@@ -142,22 +299,24 @@ const FundDetailsFee: FC = () => {
     })()
   }, [traderPool])
 
+  // Fetch price of "net. Investor Profit" in DEXE
   useEffect(() => {
     if (!priceFeed) return
     if (!baseToken || !baseToken.address) return
-    if (fundsUnderManagementBase === "0") return
+    if (fundsUnderManagementBase.isZero()) return
     ;(async () => {
       try {
-        const _fundsUnderManagementDexe =
-          await priceFeed.getNormalizedPriceOutDEXE(
-            baseToken.address,
-            fundsUnderManagementBase.toString()
-          )
+        const priceDexe = await priceFeed.getNormalizedPriceOutDEXE(
+          baseToken.address,
+          fundsUnderManagementBase.toString()
+        )
 
-        if (_fundsUnderManagementDexe && _fundsUnderManagementDexe.amountOut) {
-          setFundsUnderManagementDexe(
-            formatBigNumber(_fundsUnderManagementDexe.amountOut, 18, 3)
-          )
+        if (priceDexe && priceDexe.amountOut) {
+          const prepared: IAmount = {
+            big: priceDexe.amountOut,
+            format: formatBigNumber(priceDexe.amountOut, 18, 6),
+          }
+          setTraderDexeManagementAmount(prepared)
         }
       } catch (error) {
         console.error(error)
@@ -165,20 +324,79 @@ const FundDetailsFee: FC = () => {
     })()
   }, [baseToken, fundsUnderManagementBase, priceFeed])
 
+  // Fetch next commission epoch start date
+  useEffect(() => {
+    if (!traderPool || !account) return
+    ;(async () => {
+      try {
+        const traderData = await traderPool.getUsersInfo(account, 0, 0)
+        if (traderData && traderData[1]) {
+          const { commissionUnlockTimestamp } = traderData[1]
+          setUnlockTimestamp(commissionUnlockTimestamp)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [traderPool, account])
+
+  // Fetch platform commission percentages
+  useEffect(() => {
+    if (!coreProperties) return
+    ;(async () => {
+      try {
+        const platformCommissionPercentages =
+          await coreProperties.getDEXECommissionPercentages()
+
+        if (platformCommissionPercentages && platformCommissionPercentages[0]) {
+          setPlatformPercentageCommission({
+            big: platformCommissionPercentages[0],
+            format: formatBigNumber(platformCommissionPercentages[0], 25, 2),
+          })
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [coreProperties])
+
+  // Fetch price of funds under management in DEXE
+  useEffect(() => {
+    if (!priceFeed) return
+    ;(async () => {
+      try {
+        const priceDexe = await priceFeed.getNormalizedPriceOutDEXE(
+          "0x8a9424745056Eb399FD19a0EC26A14316684e274",
+          netInvestorProfitUSD.big.toString()
+        )
+        if (priceDexe && priceDexe.amountOut) {
+          const prepared: IAmount = {
+            big: priceDexe.amountOut,
+            format: formatBigNumber(priceDexe.amountOut, 18, 6),
+          }
+          setNetInvestorProfitDEXE(prepared)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [netInvestorProfitUSD.big, priceFeed])
+
   if (!poolData || !poolInfoData || !poolMetadata) {
     return <PageLoading />
   }
 
   return (
     <>
-      <Container>
-        <FeeDateCard>
-          <FeeDateText>
-            Performance Fee {commissionPercentage}% are available from -
-          </FeeDateText>
-        </FeeDateCard>
+      <S.Container>
+        <S.FeeDateCard>
+          <S.FeeDateText>
+            Performance Fee {poolCommissionPercentage.format}% are available
+            from {unlockDate}
+          </S.FeeDateText>
+        </S.FeeDateCard>
 
-        <MainCard>
+        <S.MainCard>
           <Flex dir="row" full>
             <Flex dir="row">
               <Icon
@@ -188,18 +406,18 @@ const FundDetailsFee: FC = () => {
                 address={poolData.id}
               />
               <div>
-                <MainCardTitle>{poolData.ticker}</MainCardTitle>
-                <MainCardDescription m="2px 0 0">
+                <S.MainCardTitle>{poolData.ticker}</S.MainCardTitle>
+                <S.MainCardDescription m="2px 0 0">
                   {poolData.name}
-                </MainCardDescription>
+                </S.MainCardDescription>
               </div>
             </Flex>
-            <MainCardHeaderRight>
-              <MainCardTitle>${totalPoolUSD}</MainCardTitle>
-              <MainCardDescription m="2px 0 0">
+            <S.MainCardHeaderRight>
+              <S.MainCardTitle>${totalPoolUSD.format}</S.MainCardTitle>
+              <S.MainCardDescription m="2px 0 0">
                 {totalPoolBase} {baseTokenSymbol}
-              </MainCardDescription>
-            </MainCardHeaderRight>
+              </S.MainCardDescription>
+            </S.MainCardHeaderRight>
           </Flex>
 
           <FeeChart address={poolAddress} />
@@ -207,54 +425,66 @@ const FundDetailsFee: FC = () => {
           <Flex full dir="column">
             <AmountRow
               title="Funds under management"
-              value={fundsUnderManagementDexe}
+              value={traderDexeManagementAmount.format}
               symbol="DEXE"
             />
             <Accordion
               title="Fund Profit (Without your funds)"
-              value="11.870"
+              value={poolProfitWithoutTraderUSD.format}
               symbol="USD"
               m="8px 0 0"
             >
               <Flex full dir="column" ai="flex-end">
-                <Amount value={"1,187.4"} symbol={"DEXE"} />
-                <Amount value={"118,74 %"} m="4px 0 0" />
+                <Amount value={"-"} symbol={"DEXE"} />
+                <Amount value={"- %"} m="4px 0 0" />
               </Flex>
             </Accordion>
             <Accordion
               title="Platform Fee"
-              value="1,068.66"
+              value={platformUSDCommission.format}
               symbol="USD"
               m="8px 0 0"
             >
               <Flex full dir="column" ai="flex-end">
                 <Amount
-                  value={platformCommissionBase}
+                  value={platformLPCommission.format}
                   symbol={baseTokenSymbol}
                 />
-                <Amount value={"30 %"} m="4px 0 0" />
+                <Amount
+                  value={`${platformPercentageCommission.format} %`}
+                  m="4px 0 0"
+                />
               </Flex>
             </Accordion>
             <Accordion
               title="Perfomance Fee"
-              value="2,493.54"
+              value={traderUSDCommission.format}
               symbol="USD"
               m="8px 0 0"
             >
               <Flex full dir="column" ai="flex-end">
-                <Amount value={traderCommissionBase} symbol={baseTokenSymbol} />
-                <Amount value={`${commissionPercentage} %`} m="4px 0 0" />
+                <Amount
+                  value={traderBaseCommission.format}
+                  symbol={baseTokenSymbol}
+                />
+                <Amount
+                  value={`${poolCommissionPercentage.format} %`}
+                  m="4px 0 0"
+                />
               </Flex>
             </Accordion>
             <Accordion
               title="Net Investor Profit"
-              value="8,309"
+              value={netInvestorProfitUSD.format}
               symbol="USD"
               m="8px 0 0"
             >
               <Flex full dir="column" ai="flex-end">
-                <Amount value={"839"} symbol={"DEXE"} />
-                <Amount value={"53,11 %"} m="4px 0 0" />
+                <Amount value={netInvestorProfitDEXE.format} symbol={"DEXE"} />
+                <Amount
+                  value={`${netInvestorProfitPercentage.format} %`}
+                  m="4px 0 0"
+                />
               </Flex>
             </Accordion>
           </Flex>
@@ -264,12 +494,15 @@ const FundDetailsFee: FC = () => {
               Request Performance Fee
             </Button>
           </Flex>
-        </MainCard>
+        </S.MainCard>
 
         <Flex dir="column" full m="40px 0 0">
-          <WithdrawalHistory payload={withdrawalHistory} />
+          <WithdrawalHistory
+            payload={withdrawalHistory}
+            unlockDate={unlockDate}
+          />
         </Flex>
-      </Container>
+      </S.Container>
     </>
   )
 }
