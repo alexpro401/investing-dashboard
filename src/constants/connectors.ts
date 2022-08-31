8173d0166670ec9da67b65cd471fe4d7068239a0
edward-arinin-web-dev <edward.arinin@gmail.com>
2022-08-31 11:52:48 +0300

    fix walletconnect RPC, fix privacy hash .env

--- src/constants/connectors.ts
+++ src/constants/connectors.ts
@@ -7,8 +7,7 @@ import walletconnectIcon from "assets/wallets/walletconnect.svg"
 
 const LOCAL_NETWORK_URL = "http://localhost:8545"
 const BSC_NETWORK_URL = "https://bsc-dataseed.binance.org/"
-const BSC_TESTNET_URL =
-  "https://speedy-nodes-nyc.moralis.io/c53d0ffcceb6907663310428/bsc/testnet"
+const BSC_TESTNET_URL = "https://data-seed-prebsc-1-s1.binance.org:8545"
 
 // const SUPPORTED_CHAINS = [1, 56, 97, 1337]
 const SUPPORTED_CHAINS = [97]
