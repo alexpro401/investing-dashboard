5df86f987b1e6ac180079f9c59e501aaa45b459e
Saymon <artem.simonenko@dexe.network>
2022-09-12 17:35:46 +0300

    disable linter for createRoot line

--- src/index.tsx
+++ src/index.tsx
@@ -74,6 +74,7 @@ const GlobalComponents = () => (
 )
 
 const container = document.getElementById("root")
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!)
 root.render(
   <React.StrictMode>
