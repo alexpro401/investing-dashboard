82b1dc8e495d4618670aed5e4cdc27ae849db20c
edward-arinin-web-dev <edward.arinin@gmail.com>
2022-09-15 17:08:51 +0300

    add Avatar, Button, Input, Switch stories

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -4,99 +4,78 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import ExpandIcon from "assets/icons/Expand"
 import Input from './index';
-
-const Page = styled.div`
-    height: fill-available;
-    overflow: auto;
-    width: fill-available;
-    max-width: 900px;
-    padding: 50px 0;
-    box-sizing: border-box;
-    display: grid; 
-    grid-template-columns: 1fr 1fr 1fr; 
-    grid-template-rows: 1fr 1fr 1fr; 
-    gap: 0px 32px; 
-    grid-template-areas: 
-    ". . ."
-    ". . ."
-    ". . .";
-`
-
-const Tile = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+import Grid from 'storybook/Grid';
 
 const Text = styled.div`
-    padding: 0 5px;
-    font-family: 'Gilroy';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 150%;
-    color: #B1C7FC;
+  padding: 0 5px;
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 150%;
+  color: #b1c7fc;
 `
 
 const Label = styled.div`
-    margin-bottom: 30px;
-    font-family: 'Gilroy';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 20px;
-    line-height: 150%;
-    color: #B1C7FC;
+  margin-bottom: 30px;
+  font-family: "Gilroy";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 150%;
+  color: #b1c7fc;
 `
 
 storiesOf('Input', module)
     .add('default', () => {
         const [value, setValue] = useState('Type something here');
 
-        return (
-            <Page>
-                <Tile>
-                    <Label>With placeholder:</Label>
-                    <Input value="" placeholder="Paste your address" />
-                </Tile>
-                <Tile>
-                    <Label>With value:</Label>
-                    <Input value="0.8291 DAI" />
-                </Tile>
-                <Tile>
-                    <Label>With label:</Label>
-                    <Input value="0.9392" label="My label here" />
-                </Tile>
-                <Tile>
-                    <Label>Interactive:</Label>
-                    <Input onChange={(v) => setValue(v)} value={value} label="My label here" />
-                </Tile>
-                <Tile>
-                    <Label>Disabled:</Label>
-                    <Input disabled value="" placeholder="Paste your address" />
-                </Tile>
-                <Tile>
-                    <Label>With grey theme:</Label>
-                    <Input value="0.8291 DAI" theme="grey" />
-                </Tile>
-                <Tile>
-                    <Label>With error border:</Label>
-                    <Input error value="0.9392" label="My label here" />
-                </Tile>
-                <Tile>
-                    <Label>Right icon:</Label>
-                    <Input rightIcon={<ExpandIcon />} value="0.9392" placeholder="text here" />
-                </Tile>
-                <Tile>
-                    <Label>Left icon:</Label>
-                    <Input leftIcon={<ExpandIcon />} value="0.9392" placeholder="text here" />
-                </Tile>
-                <Tile>
-                    <Label>Right text element:</Label>
-                    <Input rightIcon={<Text>text</Text>} value="0.9392" placeholder="text here" />
-                </Tile>
-                <Tile>
-                    <Label>Left icon:</Label>
-                    <Input leftIcon={<Text>text</Text>} value="0.9392" placeholder="text here" />
-                </Tile>
-            </Page>
-        )
+        const variants = [
+            {
+                name: "With placeholder:",
+                content: <Input value="" placeholder="Paste your address" />
+            },
+            {
+                name: "With value:",
+                content: <Input value="0.8291 DAI" />
+            },
+            {
+                name: "With label:",
+                content: <Input value="0.9392" label="My label here" />
+            },
+            {
+                name: "Interactive:",
+                content: <Input onChange={(v) => setValue(v)} value={value} label="My label here" />
+            },
+            {
+                name: "Disabled:",
+                content: <Input disabled value="" placeholder="Paste your address" />
+            },
+            {
+                name: "With grey theme:",
+                content: <Input value="0.8291 DAI" theme="grey" />
+            },
+            {
+                name: "With error border:",
+                content: <Input error value="0.9392" label="My label here" />
+            },
+            {
+                name: "Right icon:",
+                content: <Input rightIcon={<ExpandIcon />} value="0.9392" placeholder="text here" />
+            },
+            {
+                name: "Left icon:",
+                content: <Input leftIcon={<ExpandIcon />} value="0.9392" placeholder="text here" />
+            },
+            {
+                name: "Right text element:",
+                content: <Input rightIcon={<Text>text</Text>} value="0.9392" placeholder="text here" />
+            },
+            {
+                name: "Left icon:",
+                content: <Input leftIcon={<Text>text</Text>} value="0.9392" placeholder="text here" />
+            },
+        ];
+
+        return <Grid items={variants} />
     });
