812b66b5b24941671a1e88933e723120ff329454
edward-arinin-web-dev <edward.arinin@gmail.com>
2022-09-15 17:38:26 +0300

    add Slider stories

--- src/components/Slider/Slider.stories.tsx
+++ src/components/Slider/Slider.stories.tsx
@@ -2,4 +2,10 @@
 import { storiesOf } from '@storybook/react';
 import Slider from './index';
 
-// storiesOf('Slider', module).add('default', () => <Slider />);
+storiesOf('Slider', module).add('default', () => {
+    return (
+        <div style={{ width: "450px" }}>
+            <Slider name="slider" onChange={() => {}} limits={{ min: 0, max: 100 }} initial={30} />
+        </div>
+    )
+});
