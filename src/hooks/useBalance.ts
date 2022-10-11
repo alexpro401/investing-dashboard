246dbd5d2797d7b9f36bdd31416b81b0be93ad3d
edward-arinin-web-dev <edward.arinin@gmail.com>
2022-10-11 12:34:57 +0300

    improve useBalance hooks

--- src/hooks/useBalance.ts
+++ src/hooks/useBalance.ts
@@ -1,17 +1,29 @@
+import JSBI from "jsbi"
 import { Interface } from "@ethersproject/abi"
 import { ERC20 } from "abi"
-import { Token } from "interfaces"
+import { CurrencyAmount } from "lib/entities/fractions/currencyAmount"
 import { useMemo } from "react"
 import { useMultipleContractSingleData } from "state/multicall/hooks"
+import { isAddress } from "utils"
+import { Currency, Token } from "lib/entities"
 
 const ERC20_INTERFACE = new Interface(ERC20)
 
 export const useTokenBalancesWithLoadingIndicator = (
   address?: string | null,
   tokens?: (Token | undefined)[]
-) => {
-  const validatedTokenAddresses =
-    tokens?.map((token) => token?.address.toLocaleLowerCase()) || []
+): [{ [tokenAddress: string]: CurrencyAmount<Token> | undefined }, boolean] => {
+  const validatedTokens: Token[] = useMemo(
+    () =>
+      tokens?.filter(
+        (t?: Token): t is Token => isAddress(t?.address) !== false
+      ) ?? [],
+    [tokens]
+  )
+  const validatedTokenAddresses = useMemo(
+    () => validatedTokens.map((vt) => vt.address),
+    [validatedTokens]
+  )
 
   const balances = useMultipleContractSingleData(
     validatedTokenAddresses,
@@ -20,25 +32,69 @@ export const useTokenBalancesWithLoadingIndicator = (
     useMemo(() => [address || undefined], [address])
   )
 
-  console.log(balances)
-  return balances
+  const anyLoading: boolean = useMemo(
+    () => balances.some((callState) => callState.loading),
+    [balances]
+  )
+
+  return useMemo(
+    () => [
+      address && validatedTokens.length > 0
+        ? validatedTokens.reduce<{
+            [tokenAddress: string]: CurrencyAmount<Token> | undefined
+          }>((memo, token, i) => {
+            const value = balances?.[i]?.result?.[0]
+            const amount = value ? JSBI.BigInt(value.toString()) : undefined
+            if (amount) {
+              memo[token.address] = CurrencyAmount.fromRawAmount(token, amount)
+            }
+            return memo
+          }, {})
+        : {},
+      anyLoading,
+    ],
+    [address, validatedTokens, anyLoading, balances]
+  )
 }
 
 export const useTokenBalances = (
-  account?: string | null,
+  address?: string | null,
   tokens?: (Token | undefined)[]
-) => {
-  return useTokenBalancesWithLoadingIndicator(account, tokens)
+): { [tokenAddress: string]: CurrencyAmount<Token> | undefined } => {
+  return useTokenBalancesWithLoadingIndicator(address, tokens)[0]
 }
 
 export const useCurrencyBalances = (
   account?: string | null,
-  tokens?: (Token | undefined)[]
-) => {
+  currencies?: (Token | undefined)[]
+): (CurrencyAmount<Currency> | undefined)[] => {
+  const tokens = useMemo(
+    () =>
+      currencies?.filter(
+        (currency): currency is Token => currency?.isToken ?? false
+      ) ?? [],
+    [currencies]
+  )
+
   const tokenBalances = useTokenBalances(account, tokens)
-  return tokenBalances
+
+  return useMemo(
+    () =>
+      currencies?.map((currency) => {
+        if (!account || !currency) return undefined
+        if (currency.isToken) return tokenBalances[currency.address]
+        return undefined
+      }) ?? [],
+    [account, currencies, tokenBalances]
+  )
 }
 
-export const useCurrencyBalance = (account?: string | null, token?: Token) => {
-  return useCurrencyBalances(account, [token])[0]
+export const useCurrencyBalance = (
+  account?: string | null,
+  currency?: Token
+): CurrencyAmount<Currency> | undefined => {
+  return useCurrencyBalances(
+    account,
+    useMemo(() => [currency], [currency])
+  )[0]
 }
