54aa7894eedf067f1960929601ff9d5d9610bbbd
17etro <petrpetryk2016@gmail.com>
2022-10-24 20:03:56 +0300

    setup choose proposal personallity modal

---
+++ src/pages/CreateDaoProposalValidatorSelectType/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Header from "components/Header/Layout"
+
+const CreateDaoProposalValidatorSelectType: React.FC = () => {
+  return (
+    <>
+      <Header>Create proposal</Header>
+      <span style={{ color: "white" }}>
+        create validator proposal select type
+      </span>
+    </>
+  )
+}
+
+export default CreateDaoProposalValidatorSelectType
