8281d23b6490b85a3a6a331bcec228b307633745
lukachi <lukachisama@gmail.com>
2022-11-03 19:02:44 +0200

    styles for basic proposal details

--- src/pages/DaoProposalDetails/index.tsx
+++ src/pages/DaoProposalDetails/index.tsx
@@ -1,11 +1,189 @@
 import * as S from "./styled"
 
-import { FC, HTMLAttributes } from "react"
+import { FC, HTMLAttributes, useMemo, useState } from "react"
+import Header from "components/Header/Layout"
+import ExternalLink from "../../components/ExternalLink"
+import { shortenAddress } from "../../utils"
 
 interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 const DaoProposalDetails: FC<Props> = ({ ...rest }) => {
-  return <S.Root {...rest}></S.Root>
+  const TABS = useMemo(
+    () => [
+      { label: "Proposal details", number: 1 },
+      { label: "Voting settings", number: 2 },
+      { label: "Voting history", number: 3 },
+    ],
+    []
+  )
+
+  const [selectedTabNumber, setSelectedTabNumber] = useState(TABS[0].number)
+
+  return (
+    <>
+      <Header>Proposal</Header>
+      <S.DaoProposalDetails>
+        <S.DaoProposalDetailsTitleWrp>
+          <S.DaoProposalDetailsTitle>
+            Attracting more members to govern DAO
+          </S.DaoProposalDetailsTitle>
+        </S.DaoProposalDetailsTitleWrp>
+        <S.DaoProposalDetailsProgressBar />
+        <S.DaoProposalDetailsCard>
+          <p>
+            📌 Concise proposal description and reasoning It’s no secret that
+            our DAO hit a roadblock in getting proposals accepted, specifically
+            getting quorum of votes. This is mainly because the voting period is
+            only 5 days.
+          </p>
+          <p>
+            📌 Proposed solution To involve more members in voting, we discussed
+            and decided to change the following: Length of voting period from 5
+            to 10 days Minimum voting power required for voting from 100 to 50
+          </p>
+          <p>
+            📌 Conclusion Changing these parameters will allow our DAO to
+            involve more members in governance and will give them more time to
+            vote. readmore
+          </p>
+        </S.DaoProposalDetailsCard>
+        <S.DaoProposalDetailsTabs>
+          {TABS.map((el) => (
+            <S.DaoProposalDetailsTabsItem
+              key={el.number}
+              isActive={el.number === selectedTabNumber}
+              onClick={() => setSelectedTabNumber(el.number)}
+            >
+              {el.label}
+            </S.DaoProposalDetailsTabsItem>
+          ))}
+        </S.DaoProposalDetailsTabs>
+        <S.DaoProposalDetailsCard>
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="complex">
+              <p>Active settings</p>
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="success">
+              Proposed changes
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalCardRowDivider />
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="complex">
+              <span>Length of voting period</span>
+              <p>1D/1H/1M</p>
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="success">
+              10D/1H/1M
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalCardRowDivider />
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="complex">
+              <span>Min. voting power required for voting </span>
+              <p>100</p>
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="success">
+              50
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+        </S.DaoProposalDetailsCard>
+        <S.DaoProposalDetailsCard>
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              Contract address
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              <ExternalLink href={""}>
+                {shortenAddress("0x987654321234567898765432123456789876543")}
+              </ExternalLink>
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalCardRowDivider />
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              value
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              0.2 BNB
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+        </S.DaoProposalDetailsCard>
+        <S.DaoProposalDetailsCard>
+          <S.DaoProposalDetailsCardTitle>
+            Proposal Details
+          </S.DaoProposalDetailsCardTitle>
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              created
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              <ExternalLink href={""}>
+                {shortenAddress("0x987654321234567898765432123456789876543")}
+              </ExternalLink>
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              voting status
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              <S.DaoProposalDetailsRowText type="value">
+                10.000/
+              </S.DaoProposalDetailsRowText>
+              <S.DaoProposalDetailsRowText type="label">
+                80.000
+              </S.DaoProposalDetailsRowText>
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              Proposal type
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              Changing voting options
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              Addresses voted
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              1
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              My votes
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              200
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+
+          <S.DaoProposalDetailsRow>
+            <S.DaoProposalDetailsRowText type="label">
+              Voted via delegate
+            </S.DaoProposalDetailsRowText>
+            <S.DaoProposalDetailsRowText type="value">
+              0
+            </S.DaoProposalDetailsRowText>
+          </S.DaoProposalDetailsRow>
+        </S.DaoProposalDetailsCard>
+      </S.DaoProposalDetails>
+    </>
+  )
 }
 
 export default DaoProposalDetails
--- src/pages/DaoProposalDetails/styled.tsx
+++ src/pages/DaoProposalDetails/styled.tsx
@@ -1,3 +1,168 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-export const Root = styled.div``
+export const DaoProposalDetails = styled.div`
+  overflow: hidden auto;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  color: ${(props) => props.theme.textColors.primary};
+  padding: 16px;
+  flex: 1;
+  height: calc(100vh - 94px);
+  @media all and (display-mode: standalone) {
+    height: calc(100vh - 115px);
+  }
+`
+
+export const DaoProposalDetailsTitleWrp = styled.div``
+
+export const DaoProposalDetailsTitle = styled.div``
+
+export const DaoProposalDetailsProgressBar = styled.div`
+  overflow: hidden;
+  background: #131927;
+  border-radius: 6px;
+  height: 3px;
+  width: 100%;
+
+  &:after {
+    content: "";
+    display: block;
+    height: 100%;
+    width: 50%;
+    background: ${(props) => props.theme.statusColors.success};
+  }
+`
+
+export const DaoProposalDetailsTabs = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background: rgba(32, 41, 58, 0.5);
+  }
+`
+
+export const DaoProposalDetailsTabsItem = styled.button.attrs(() => ({
+  type: "button",
+}))<{ isActive: boolean }>`
+  position: relative;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 15px;
+  text-align: center;
+  padding-bottom: 12px;
+  background: transparent;
+  border: none;
+  color: ${(props) =>
+    props.isActive
+      ? props.theme.statusColors.success
+      : props.theme.textColors.secondary};
+
+  ${(props) =>
+    props.isActive
+      ? css`
+          &:after {
+            content: "";
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 1px;
+            background: ${(props) => props.theme.statusColors.success};
+            z-index: 1;
+          }
+        `
+      : ""}
+`
+
+export const DaoProposalDetailsCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  background: #181e2c;
+  border-radius: 20px;
+  padding: 16px;
+`
+
+export const DaoProposalDetailsCardTitle = styled.span`
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 16px;
+  color: ${(props) => props.theme.textColors.primary};
+  margin-bottom: 12px;
+`
+
+export const DaoProposalDetailsRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 16px;
+`
+
+export const DaoProposalDetailsRowText = styled.span<{
+  type: "label" | "value" | "complex" | "success" | "error" | "warning" | "info"
+}>`
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 150%;
+
+  ${(props) => {
+    if (props.type === "complex") {
+      return css`
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+
+        span {
+          color: ${(props) => props.theme.textColors.secondary};
+        }
+
+        p {
+          color: ${(props) => props.theme.textColors.primary};
+        }
+      `
+    } else if (props.type === "label") {
+      return css`
+        color: ${(props) => props.theme.textColors.secondary};
+      `
+    } else if (props.type === "value") {
+      return css`
+        color: ${(props) => props.theme.textColors.primary};
+      `
+    } else if (props.type === "success") {
+      return css`
+        color: ${(props) => props.theme.textColors.success};
+      `
+    } else if (props.type === "error") {
+      return css`
+        color: ${(props) => props.theme.textColors.error};
+      `
+    } else if (props.type === "warning") {
+      return css`
+        color: ${(props) => props.theme.textColors.warning};
+      `
+    } else if (props.type === "info") {
+      return css`
+        color: ${(props) => props.theme.textColors.info};
+      `
+    } else {
+      return ""
+    }
+  }}
+`
+
+export const DaoProposalCardRowDivider = styled.div`
+  background: rgba(32, 41, 58, 0.6);
+  opacity: 0.5;
+  width: 100%;
+  height: 1px;
+`
