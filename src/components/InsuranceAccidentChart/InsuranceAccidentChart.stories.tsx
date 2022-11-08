36efad740e65870507586984b6cac309b6dc6d2e
Saymon <artem.simonenko@dexe.network>
2022-11-08 16:03:14 +0200

    Removed unused components

--- src/components/InsuranceAccidentChart/InsuranceAccidentChart.stories.tsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-import { storiesOf } from '@storybook/react';
-import InsuranceAccidentChart from './index';
-
-// storiesOf('InsuranceAccidentChart', module).add('default', () => <InsuranceAccidentChart />);
