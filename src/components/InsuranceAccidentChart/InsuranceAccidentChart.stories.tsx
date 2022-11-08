7b969b6c07462a440a762bf5513ef5aa84f4c198
Saymon <artem.simonenko@dexe.network>
2022-11-08 11:35:31 +0200

    Fix naming

---
+++ src/components/InsuranceAccidentChart/InsuranceAccidentChart.stories.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable */
+import { storiesOf } from '@storybook/react';
+import InsuranceAccidentChart from './index';
+
+// storiesOf('InsuranceAccidentChart', module).add('default', () => <InsuranceAccidentChart />);
