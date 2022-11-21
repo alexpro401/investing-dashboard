4b2eb2235b979d02698ebbf0d97e06efe504a546
Saymon <artem.simonenko@dexe.network>
2022-11-21 19:48:29 +0200

    replaced old card with new

--- src/pages/DaoPoolsList/index.tsx
+++ src/pages/DaoPoolsList/index.tsx
@@ -1,12 +1,11 @@
 import * as React from "react"
-import { isEmpty, isNil } from "lodash"
+import { isEmpty } from "lodash"
 import { useSelector } from "react-redux"
 import { useWeb3React } from "@web3-react/core"
-import { PulseSpinner } from "react-spinners-kit"
 import AutoSizer from "react-virtualized-auto-sizer"
-import { areEqual, FixedSizeList } from "react-window"
+import { FixedSizeList, ListChildComponentProps } from "react-window"
 
-import { Text, To } from "theme"
+import theme, { Text, Center } from "theme"
 import { Container, Indents, Action, List } from "./styled"
 
 import Header from "components/Header/Layout"
@@ -14,17 +13,14 @@ import RouteTabs from "components/RouteTabs"
 import { ITab } from "interfaces"
 import tutorialImageSrc from "assets/others/create-fund-docs.png"
 import TutorialCard from "components/TutorialCard"
-import {
-  selectGovPoolsByNameSubstring,
-  selectPayload,
-} from "state/govPools/selectors"
-import GovPoolStatisticCard from "components/cards/GovPoolStatistic"
+import { selectGovPoolsByNameSubstring } from "state/govPools/selectors"
+import { IGovPoolQuery } from "interfaces/thegraphs/gov-pools"
+import PoolRow from "./components/PoolRow"
 
 const DaoPoolsList: React.FC = () => {
   const { account } = useWeb3React()
 
   const search = React.useState("")
-  const { loading } = useSelector(selectPayload)
   const pools = useSelector((s) => selectGovPoolsByNameSubstring(s, search[0]))
 
   const tabs: ITab[] = [
@@ -37,44 +33,6 @@ const DaoPoolsList: React.FC = () => {
       source: `/dao/list/voting-power`,
     },
   ]
-
-  const PoolRow = ({ data, index, style }) => {
-    const pool = data[index]
-
-    return (
-      <Indents style={style}>
-        <To to={`/dao/${pool.id}`}>
-          <GovPoolStatisticCard data={pool} account={account} index={index} />
-        </To>
-      </Indents>
-    )
-  }
-
-  const PoolsList = React.useMemo(() => {
-    if (loading && (isEmpty(pools) || isNil(pools))) {
-      return <PulseSpinner />
-    }
-
-    if (!loading && isEmpty(pools)) {
-      return <Text color="#B1C7FC">No DAO pools</Text>
-    }
-
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <FixedSizeList
-            itemData={pools}
-            itemCount={pools.length}
-            itemSize={136 + 16}
-            height={height}
-            width={width}
-          >
-            {React.memo(PoolRow, areEqual)}
-          </FixedSizeList>
-        )}
-      </AutoSizer>
-    )
-  }, [pools, loading])
 
   return (
     <>
@@ -95,9 +53,29 @@ const DaoPoolsList: React.FC = () => {
               <Action text="+ Create new" routePath="/create-fund" />
             </List.Header>
           </Indents>
-          <List.Scroll center={isNil(pools) || isEmpty(pools)}>
-            {PoolsList}
-          </List.Scroll>
+          <div>
+            {!isEmpty(pools) ? (
+              <AutoSizer>
+                {({ height, width }) => (
+                  <FixedSizeList
+                    itemData={pools}
+                    itemCount={pools.length}
+                    itemSize={144}
+                    height={height}
+                    width={width}
+                  >
+                    {(payload: ListChildComponentProps<IGovPoolQuery[]>) => (
+                      <PoolRow payload={payload} account={account} />
+                    )}
+                  </FixedSizeList>
+                )}
+              </AutoSizer>
+            ) : (
+              <Center>
+                <Text color={theme.textColors.secondary}>No DAO pools yet</Text>
+              </Center>
+            )}
+          </div>
         </List.Container>
       </Container>
     </>
