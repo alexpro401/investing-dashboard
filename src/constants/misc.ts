7526250eae7494cd93841cb95871a24b2c788e75
Saymon <artem.simonenko@dexe.network>
2022-12-19 14:09:54 +0200

    Add MAX_PAGINATION_COUNT

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -5,6 +5,7 @@ export const TRANSACTION_PENDING_DIVIDER = 60000
 
 // Default count of loading collection items
 export const DEFAULT_PAGINATION_COUNT = 10
+export const MAX_PAGINATION_COUNT = 1000
 
 // Default timeout before alert will be hide
 export const DEFAULT_ALERT_HIDDEN_TIMEOUT = 5000
