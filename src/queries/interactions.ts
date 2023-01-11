89566d44470895f579114409833853195a2481ac
Saymon <artem.simonenko@dexe.network>
2023-01-11 16:19:32 +0200

    divided UserTransactionsQuery into smaller pieces

--- src/queries/interactions.ts
+++ src/queries/interactions.ts
@@ -1,3 +1,27 @@
+const TRANSACTION_DATA = `
+	id
+  type
+  user
+  timestamp
+  interactionsCount
+`
+
+const TRANSACTION_DATA_NON_PERMANENT = `
+  exchange { id fromToken toToken }
+  vest { id pool baseAmount }
+  poolCreate { id }
+  proposalEdit { id }
+  riskyProposalCreate { id }
+  riskyProposalExchange { id }
+  riskyProposalVest { id }
+  investProposalClaimSupply { id }
+  investProposalCreate { id }
+  investProposalWithdraw { id }
+  insuranceStake { id }
+  getPerfomanceFee { id }
+  onlyPool { id }
+`
+
 export const UserTransactionsQuery = `
   query($offset: Int!, $limit: Int!, $address: String!, $transactionTypes: [Int]!) {
     transactions(
@@ -5,25 +29,8 @@ export const UserTransactionsQuery = `
       where: {user: $address, type_contains: $transactionTypes}, 
       orderBy: timestamp, orderDirection: desc
     ) {
-      id
-      timestamp
-      type
-      user
-      interactionsCount
-
-      exchange { id fromToken toToken }
-      vest { id pool baseAmount }
-      poolCreate { id }
-      proposalEdit { id }
-      riskyProposalCreate { id }
-      riskyProposalExchange { id }
-      riskyProposalVest { id }
-      investProposalClaimSupply { id }
-      investProposalCreate { id }
-      investProposalWithdraw { id }
-      insuranceStake { id }
-      getPerfomanceFee { id }
-      onlyPool { id }
+      ${TRANSACTION_DATA}
+      ${TRANSACTION_DATA_NON_PERMANENT}
     }
   }
 `
