31a7279e43cac0c2dd09e401d75aba4594892feb
lukachi <lukachisama@gmail.com>
2023-01-19 13:26:53 +0200

    added localization with helper example, (en)

--- src/localization/helpers/localizePoolType.ts
+++ src/localization/helpers/localizePoolType.ts
@@ -1,11 +1,12 @@
 import { PoolType } from "consts"
+import i18n from "localization"
 
 export const localizePoolType = (poolType?: PoolType) => {
   if (!poolType) return ""
 
   return {
-    ALL_POOL: "All Pool",
-    INVEST_POOL: "Invest Pool",
-    BASIC_POOL: "Basic Pool",
+    ALL_POOL: i18n.t("filters.pool-type.all-pool"),
+    INVEST_POOL: i18n.t("filters.pool-type.invest-pool"),
+    BASIC_POOL: i18n.t("filters.pool-type.basic-pool"),
   }[poolType]
 }
--- src/localization/index.ts
+++ src/localization/index.ts
@@ -1,1 +1,28 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+
+import resources from "./resources"
+
+const STORAGE_KEY = "react-template"
+const DEFAULT_LOCALE = "en"
+
+const locale = localStorage?.getItem(STORAGE_KEY) ?? DEFAULT_LOCALE
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    returnNull: false
+  }
+}
+
+i18n.use(initReactI18next).init({
+  fallbackLng: locale,
+  lng: locale,
+  resources,
+  interpolation: {
+    escapeValue: false, // not needed for react as it escapes by default
+  },
+  returnNull: false,
+})
+
+export default i18n
 export * from "./helpers"
