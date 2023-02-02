90aa3f417f047febb0ee7a7fff7f16676df455fe
17etro <petrpetryk2016@gmail.com>
2023-02-02 21:44:41 +0200

    complete dao proposals for dao profile

--- src/pages/DaoProfile/components/AllProposalsList/index.tsx
+++ src/pages/DaoProfile/components/AllProposalsList/index.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useMemo } from "react"
+import { useParams } from "react-router-dom"
+
+import { DaoProposalsList } from "common"
 
 import * as S from "./styled"
 
@@ -9,6 +12,8 @@ type EndedLevelTab = "passed" | "rejected"
 type CompletedLevelTab = "all" | "rewards"
 
 const AllProposalsList: React.FC = () => {
+  const { daoAddress } = useParams()
+
   const [firstLevelSelectedTab, setFirstLevelSelectedTab] =
     useState<FirstLevelTab>("opened")
   const [endedSelectedTab, setEndedSelectedTab] =
@@ -70,9 +75,46 @@ const AllProposalsList: React.FC = () => {
   )
 
   return (
-    <>
+    <S.Root>
       <S.HeaderTabsWrp tabs={FIRST_LEVEL_TABS} />
-    </>
+      {firstLevelSelectedTab === "opened" && (
+        <DaoProposalsList govPoolAddress={daoAddress} status="opened" />
+      )}
+      {firstLevelSelectedTab === "ended" && (
+        <>
+          <S.PageSubTabs tabs={ENDED_TABS} />
+          {endedSelectedTab === "passed" && (
+            <DaoProposalsList
+              govPoolAddress={daoAddress}
+              status="ended-passed"
+            />
+          )}
+          {endedSelectedTab === "rejected" && (
+            <DaoProposalsList
+              govPoolAddress={daoAddress}
+              status="ended-rejected"
+            />
+          )}
+        </>
+      )}
+      {firstLevelSelectedTab === "completed" && (
+        <>
+          <S.PageSubTabs tabs={COMPLETED_TABS} />
+          {completedSelectedTab === "all" && (
+            <DaoProposalsList
+              govPoolAddress={daoAddress}
+              status="completed-all"
+            />
+          )}
+          {completedSelectedTab === "rewards" && (
+            <DaoProposalsList
+              govPoolAddress={daoAddress}
+              status="completed-rewards"
+            />
+          )}
+        </>
+      )}
+    </S.Root>
   )
 }
 
--- src/pages/DaoProfile/components/AllProposalsList/styled.tsx
+++ src/pages/DaoProfile/components/AllProposalsList/styled.tsx
@@ -1,7 +1,23 @@
 import styled from "styled-components/macro"
 
 import HeaderTabs from "components/Header/Tabs"
+import RouteTabs from "components/RouteTabs"
+import { respondTo } from "theme"
+
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`
 
 export const HeaderTabsWrp = styled(HeaderTabs)`
   justify-content: flex-start;
+`
+
+export const PageSubTabs = styled(RouteTabs)`
+  ${respondTo("sm")} {
+    width: max-content;
+    min-width: 350px;
+  }
 `
