9479d24ddb6358c04c34978a21a2aa201af7e25f
lukachi <lukachisama@gmail.com>
2023-02-13 17:36:51 +0200

    dao profile balance tab markup

--- src/pages/DaoProfile/tabs/desktop/MyBalance/index.tsx
+++ src/pages/DaoProfile/tabs/desktop/MyBalance/index.tsx
@@ -1,15 +1,32 @@
 import React from "react"
 
+import { PoolStatisticsItem } from "common"
+import { formatNumber, normalizeBigNumber } from "utils"
+import { useTranslation } from "react-i18next"
+
 import onChainImg from "assets/images/onchain-voting.png"
+
 import * as SCommon from "../styled"
 import * as S from "./styled"
 
 const MyBalance: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
     <S.Root>
+      <S.TutorialBlock>
+        <S.TutorialTitle>
+          {t("dao-profile.my-balance.tutorial-title")}
+        </S.TutorialTitle>
+        <S.TutorialBtn>
+          {t("dao-profile.my-balance.tutorial-btn")}
+        </S.TutorialBtn>
+      </S.TutorialBlock>
       <S.OnChainVotingSection>
         <S.OnChainVotingSectionHeader>
-          <SCommon.SectionTitle>Onchain voting</SCommon.SectionTitle>
+          <SCommon.SectionTitle>
+            {t("dao-profile.my-balance.section-1-title")}
+          </SCommon.SectionTitle>
           <S.OnChainTutorialLink
             href="https://github.com/"
             target="_blank"
@@ -20,10 +37,43 @@ const MyBalance: React.FC = () => {
         </S.OnChainVotingSectionHeader>
         <S.OnChainVotingContent>
           <img src={onChainImg} alt="" />
-          <div />
+
+          <S.OnChainVotingContentStatistics>
+            <PoolStatisticsItem
+              label={t("dao-profile.my-balance.total-voting-power-lbl")}
+              value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+              tooltipMsg={"Lorem ipsum dolor sit amet!"}
+            />
+            <PoolStatisticsItem
+              label={t("dao-profile.my-balance.received-rewards-lbl")}
+              value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+              tooltipMsg={"Lorem ipsum dolor sit amet!"}
+            />
+            <PoolStatisticsItem
+              label={t("dao-profile.my-balance.claim-available-lbl")}
+              value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+              tooltipMsg={"Lorem ipsum dolor sit amet!"}
+            />
+
+            <PoolStatisticsItem
+              label={t("dao-profile.my-balance.proposal-voting-history-lbl")}
+              value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+              tooltipMsg={"Lorem ipsum dolor sit amet!"}
+            />
+            <PoolStatisticsItem
+              label={t("dao-profile.my-balance.delegation-fee-lbl")}
+              value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+              tooltipMsg={"Lorem ipsum dolor sit amet!"}
+            />
+            <PoolStatisticsItem
+              label={t("dao-profile.my-balance.apr-lbl")}
+              value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+              tooltipMsg={"Lorem ipsum dolor sit amet!"}
+            />
+          </S.OnChainVotingContentStatistics>
           <S.OnChainClaimButtonWrp>
             <S.OnChainClaimButton
-              text="Claim all"
+              text={t("dao-profile.my-balance.claim-btn")}
               color="tertiary"
               onClick={() => {}}
               size="small"
@@ -31,6 +81,39 @@ const MyBalance: React.FC = () => {
           </S.OnChainClaimButtonWrp>
         </S.OnChainVotingContent>
       </S.OnChainVotingSection>
+      <S.BalanceSection>
+        <S.BalanceSectionTitle>
+          {t("dao-profile.my-balance.section-2-title")}
+        </S.BalanceSectionTitle>
+        <S.BalanceSectionItem>
+          <S.BalanceBaseToken
+            tokenAddress={""}
+            label={t("dao-profile.my-balance.balance-total-lbl")}
+            value={formatNumber("5000", 2)}
+          />
+          <PoolStatisticsItem
+            label={t("dao-profile.my-balance.used-in-pool-lbl", {
+              poolSymbol: "PLPLPLPL",
+            })}
+            value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+            tooltipMsg={"Lorem ipsum dolor sit amet!"}
+          />
+          <PoolStatisticsItem
+            label={t("dao-profile.my-balance.delegated-lbl")}
+            value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+            tooltipMsg={"Lorem ipsum dolor sit amet!"}
+          />
+          <PoolStatisticsItem
+            label={t("dao-profile.my-balance.available-to-withdraw-lbl")}
+            value={`$${normalizeBigNumber("5000000000000000000", 18, 2)}`}
+            tooltipMsg={"Lorem ipsum dolor sit amet!"}
+          />
+
+          <S.BalanceSectionItemBtn
+            text={t("dao-profile.my-balance.withdraw-btn")}
+          />
+        </S.BalanceSectionItem>
+      </S.BalanceSection>
     </S.Root>
   )
 }
--- src/pages/DaoProfile/tabs/desktop/MyBalance/styled.tsx
+++ src/pages/DaoProfile/tabs/desktop/MyBalance/styled.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components/macro"
 
 import theme, { Flex, respondTo } from "theme"
 import { AppButton } from "common"
+import { PoolBaseToken } from "pages/PoolProfile/components" // FIXME
 
 export const Root = styled(Flex).attrs(() => ({
   full: true,
@@ -9,6 +10,34 @@ export const Root = styled(Flex).attrs(() => ({
   dir: "column",
 }))`
   padding-bottom: 24px;
+`
+
+export const TutorialBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 46px 128px;
+  width: 100%;
+  margin: 64px 0 0;
+  min-height: 135px;
+  background: linear-gradient(180deg, #0c1018 0%, #0c1018 100%);
+  border-radius: 20px;
+`
+
+export const TutorialTitle = styled.h2`
+  font-style: normal;
+  font-size: 36px;
+  line-height: 1.2;
+  font-weight: 400;
+  letter-spacing: -0.02em;
+  color: #ffffff;
+  margin: 0;
+`
+
+export const TutorialBtn = styled.button`
+  background: none;
+  border: none;
+  color: ${(props) => props.theme.brandColors.secondary};
 `
 
 export const OnChainVotingSection = styled(Flex).attrs(() => ({
@@ -33,15 +62,22 @@ export const OnChainTutorialLink = styled.a`
   text-decoration: none;
 `
 
-export const OnChainVotingContent = styled(Flex).attrs(() => ({
-  full: true,
-  ai: "center",
-  gap: "32",
-}))`
+export const OnChainVotingContent = styled.div`
+  display: flex;
+  align-items: center;
   border-radius: 20px;
   background-color: #0c1018;
+  width: 100%;
   border: 2px dashed #336683;
   padding: 0 24px;
+`
+
+export const OnChainVotingContentStatistics = styled.div`
+  flex: 1;
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: var(--app-gap);
 `
 
 export const OnChainClaimButtonWrp = styled(Flex).attrs(() => ({
@@ -51,8 +87,42 @@ export const OnChainClaimButtonWrp = styled(Flex).attrs(() => ({
 
 export const OnChainClaimButton = styled(AppButton)`
   min-width: 150px;
+  border: none;
 
   ${respondTo("lg")} {
     min-width: 200px;
   }
 `
+
+export const BalanceSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: var(--app-gap);
+  width: 100%;
+`
+
+export const BalanceSectionTitle = styled.h2`
+  font-size: 20px;
+  line-height: 1.2;
+  font-weight: 900;
+  letter-spacing: -0.03em;
+  color: ${(props) => props.theme.textColors.primary};
+`
+
+export const BalanceSectionItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background: #0c1018;
+  border-radius: 20px;
+  padding: var(--app-padding);
+  gap: var(--app-gap);
+`
+
+export const BalanceSectionItemBtn = styled(AppButton).attrs({
+  color: "secondary",
+})``
+
+export const BalanceBaseToken = styled(PoolBaseToken)``
